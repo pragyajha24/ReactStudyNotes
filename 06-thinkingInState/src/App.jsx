23fcd786947c84107dbcfbc34960edd3fc7237b8
Pragya Jha <pragyajha50@gmail.com>
2026-01-20 15:48:17 +0530

    updated files

--- 06-thinkingInState/src/App.jsx
+++ 06-thinkingInState/src/App.jsx
@@ -1,35 +1,125 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./index.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  // adding new items to the state
+  function handleAddItems(item) {
+    setItems(function (items) {
+      return [...items, item];
+    });
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
+      <Stats />
+    </div>
+  );
 }
 
-export default App
+function Logo() {
+  return <h1> 🏝️ Far Away 👜 </h1>;
+}
+
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  // //moving this state to parent component
+  // const [items, setItems] = useState([]);
+
+  // // adding new items to the state
+  // function handleAddItems(item) {
+  //   setItems(function (items) {
+  //     return [...items, item];
+  //   });
+  // }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    // handleAddItems(newItem);
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip ?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, function (_, i) {
+          return i + 1;
+        }).map(function (num) {
+          return (
+            <option value={num} key={num}>
+              {" "}
+              {num}
+            </option>
+          );
+        })}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList({ items }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map(function (item) {
+          return <Item item={item} key={item.id} />;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+// item is the destructed  prop name
+//function Item({item}){}
+function Item(props) {
+  return (
+    <li>
+      <span style={props.item.packed ? { textDecoration: "line-through" } : {}}>
+        {props.item.quantity} {props.item.description}{" "}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        {" "}
+        👜 You have X items on your lists, and you already packed X (X%){" "}
+      </em>
+    </footer>
+  );
+}
