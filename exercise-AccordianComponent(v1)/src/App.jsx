58d1623571ce6783a794003e25a7fb435839fcc6
Pragya Jha <pragyajha50@gmail.com>
2026-01-26 22:17:43 +0530

    created a new file for exercise  accordion

---
+++ exercise-AccordianComponent(v1)/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react"
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+
+export default function App(){
+  return (
+    <div>
+    <Accordion  />
+    </div>
+  )
+}
+
+function Accordion(){
+
+ const [item,setItem] = useState([]);
+
+  return (
+  <div className="accordion">
+    <ul className='content-box'>
+    {
+      faqs.map(function(faq){
+        return <Item faq={faq}  key={faq.title}/>
+      })
+    }
+    </ul>
+    </div>
+  )
+}
+
+
+function Item({faq}){
+  return (
+    <div className="item">
+    
+    <p className="number"> </p>
+    <p className="title">{faq.title}</p>
+     <p className="icon">+</p>
+    <p className="text">{faq.text}</p>
+   
+    
+    
+    </div>
+  )
+}
