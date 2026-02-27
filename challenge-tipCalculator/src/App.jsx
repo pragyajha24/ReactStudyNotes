ad1ef7d2448eb417151ce770aa98ad4166a4c8a0
Pragya Jha <pragyajha50@gmail.com>
2026-02-27 15:23:48 +0530

    added state for input and select element, moved the state to parent component

--- challenge-tipCalculator/src/App.jsx
+++ challenge-tipCalculator/src/App.jsx
@@ -9,23 +9,33 @@ export default function App() {
 }
 
 function TipCalculator() {
+
+
+  // lifting the state
+ const [bill, setBill] = useState("");
+   const [tip, setTip] = useState(0);
+
   return (
     <div>
       <h1>Tip calculator</h1>
-      <Bill />
-      <Tip>
+      <Bill  bill={bill} setBill={setBill} />
+      <Tip tip={tip} setTip={setTip} >
         <p>How did you like the service ?</p>
       </Tip>
 
-      <Tip>
+      <Tip tip={tip} setTip={setTip}>
         <p>How did your friend like the service ? </p>{" "}
       </Tip>
+
+      <TotalBill bill={bill} tip={tip} />
+
+      <Reset />
     </div>
   );
 }
 
-function Bill() {
-  const [bill, setBill] = useState("");
+function Bill({bill,setBill}) {
+  // const [bill, setBill] = useState("");
 
   return (
     <div>
@@ -39,8 +49,8 @@ function Bill() {
   );
 }
 
-function Tip({ children }) {
-  const [tip, setTip] = useState(0);
+function Tip({ children,tip ,setTip}) {
+  // const [tip, setTip] = useState(0);
 
   return (
     <div>
@@ -56,10 +66,24 @@ function Tip({ children }) {
   );
 }
 
-function TotalBill() {
+function TotalBill({bill,tip}) {
   return (
     <div>
-      <h2></h2>
+      <h2>
+      You pay({bill} + ${tip})
+      
+      </h2>
     </div>
   );
 }
+
+function Reset(){
+  return (
+    <button>
+    Reset
+    </button>
+  )
+}
+
+
+
