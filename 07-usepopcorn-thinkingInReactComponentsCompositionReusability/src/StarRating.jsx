67c1cc3754614bcad12842b7782a5df0813b86a8
Pragya Jha <pragyajha50@gmail.com>
2026-04-07 14:52:20 +0530

    Created star component and called it under star ratng component

--- 07-usepopcorn-thinkingInReactComponentsCompositionReusability/src/StarRating.jsx
+++ 07-usepopcorn-thinkingInReactComponentsCompositionReusability/src/StarRating.jsx
@@ -6,38 +6,65 @@
 
 */
 
-
- const containerStyle ={
-        display:'flex',
-        alignItems: 'center',
-        gap:'16px'
-    }
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
 
 const starContainerStyle = {
-    display: 'flex',
-    gap: '4px',
-}
+  display: "flex",
+  gap: "4px",
+};
 
-const textStyle ={
-    lineHeight : '1',
-    margin: '0'
-}
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
 
- {/* if someone used this component without specifing max rating property , so to solve this  case we will define default value of the property*/}
-// In JS whenever we destructure an object , we can set a default value 
+{
+  /* if someone used this component without specifing max rating property , so to solve this  case we will define default value of the property*/
+}
+// In JS whenever we destructure an object , we can set a default value
 //here we are destructing the prop object
-export default function StarRating({maxRating = 5}){  
-    return (
-        <div style={containerStyle}>
+export default function StarRating({ maxRating = 5 }) {
+  return (
+    <div style={containerStyle}>
+      {/* dynamically generating the star elements instead of writing with hand , that's the only way we could have sometimes 5 stars or 10 stars or any other number*/}
+      <div style={starContainerStyle}>
+        {/*  creates a empty array with 5 elements that we can immediately loop over, by passing a function */}
+        {/* i+1 as i is zero based (Array)  */}
 
-    {/* dynamically generating the star elements instead of writing with hand , that's the only way we could have sometimes 5 stars or 10 stars or any other number*/ }
-        <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          //<span>S{i+1}</span>
+          <Star key={i} />
+        ))}
+      </div>
+      <p style={textStyle}>10 </p>
+    </div>
+  );
+}
 
-  {/*  creates a empty array with 5 elements that we can immediately loop over, by passing a function */ }
-  {/* i+1 as i is zero based (Array)  */}
-        {Array.from({length:maxRating}, (_,i) => <span>S{i+1} </span>)}
-         </div>
-        <p style={textStyle}>10 </p>
-         </div>
-    )
-} 
+const starStyle = {
+  width: "48px",
+  height: "48px",
+  display: "block",
+  cursor: "pointer",
+};
+
+// creating stars
+//called this under star rating component
+function Star() {
+  return (
+    <span role="button" style={starStyle}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="#000"
+        stroke="#000"
+      >
+        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+      </svg>
+    </span>
+  );
+}
