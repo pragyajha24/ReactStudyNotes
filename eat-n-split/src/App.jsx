298c92d05c6aee089503846886f425068387be84
Pragya Jha <pragyajha50@gmail.com>
2026-05-21 19:07:47 +0530

    selecting a friend functionality,data from friend list component to pass info in split bll component, both of these need to commnunicate, created new state for selectedFriend in app component, as these two are sbling component. And when no friend is selected , hide the form split bill component , so conditionally redner this form

--- eat-n-split/src/App.jsx
+++ eat-n-split/src/App.jsx
@@ -25,54 +25,67 @@ const initialFriends = [
 ];
 
 export default function App() {
+  // state for friend object
+  const [selectedFriend, setSelectedFriend] = useState(null);
 
   //state for adding and updating new object in the list
   const [friends, setFriends] = useState(initialFriends);
 
   // state for displaying the Form add friend component
-  const [showAddFriend,setShowAddFriend] = useState(false);
+  const [showAddFriend, setShowAddFriend] = useState(false);
 
   //event handler function to display the show add friend form
   //toggle the current state
-  function handleShowAddFriend(){
-   setShowAddFriend(function(show){
-    return !show;
-   })
+  function handleShowAddFriend() {
+    setShowAddFriend(function (show) {
+      return !show;
+    });
   }
 
   // to add new friends
-  //friend - new friend that we created 
-  function handleAddFriend(friend){
+  //friend - new friend that we created
+  function handleAddFriend(friend) {
     //take the current friends - then we create  a new array
     //spread current friends and add new friend
-    setFriends(function(friends){
-      return [...friends,friend]
-    })
+    setFriends(function (friends) {
+      return [...friends, friend];
+    });
 
-    //to close the add friend form after submiting it 
+    //to close the add friend form after submiting it
     setShowAddFriend(false);
   }
 
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendList friends={friends} Button={Button}  initialFriends={initialFriends} />
+        <FriendList
+          friends={friends}
+          Button={Button}
+          initialFriends={initialFriends}
+        />
 
-          {/* conditionally rendering the component */}
-     { showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} Button={Button}   /> }
+        {/* conditionally rendering the component */}
+        {showAddFriend && (
+          <FormAddFriend onAddFriend={handleAddFriend} Button={Button} />
+        )}
 
-           {/* change text from Add friend to Close depending on state value */}
-        <Button onClick={handleShowAddFriend} > {showAddFriend ? 'Close' : 'Add Friend'} </Button>
+        {/* change text from Add friend to Close depending on state value */}
+        <Button onClick={handleShowAddFriend}>
+          {" "}
+          {showAddFriend ? "Close" : "Add Friend"}{" "}
+        </Button>
       </div>
 
-      <FormSplitBill Button={Button}  />
+      {selectedFriend && <FormSplitBill Button={Button} />}
     </div>
   );
 }
 
 // reusable component
- function Button({children, onClick}){
+function Button({ children, onClick }) {
   return (
-    <button className="button" onClick={onClick}>{children} </button>
-  )
+    <button className="button" onClick={onClick}>
+      {children}{" "}
+    </button>
+  );
 }
--- eat-n-split/src/FriendList.jsx
+++ eat-n-split/src/FriendList.jsx
@@ -1,34 +1,39 @@
-
-
-
-export default function FriendList({Button , friends}){
+export default function FriendList({ Button, friends }) {
   return (
-    <ul className="list"> 
-    
-     {friends.map(function(friend){
-      return <List friend={friend} Button={Button} key={friend.id} />
-     }) }
-
+    <ul className="list">
+      {friends.map(function (friend) {
+        return <List friend={friend} Button={Button} key={friend.id} />;
+      })}
     </ul>
-  )
+  );
 }
 
-function List({friend,Button}){
+function List({ friend, Button }) {
   return (
-    <li> 
+    <li>
       <img src={friend.image} />
-    
-    <div> 
-      <p> {friend.name} </p>
- 
-      {friend.balance > 0 && <p className="green"> {friend.name} owes you {Math.abs(friend.balance)} </p>}
 
-      {friend.balance < 0 && <p className="red"> You owe {friend.name} {Math.abs(friend.balance)} </p>}
+      <div>
+        <p> {friend.name} </p>
 
-      {friend.balance === 0 && <p> You and {friend.name} are even. </p>}
-   </div>
-     
+        {friend.balance > 0 && (
+          <p className="green">
+            {" "}
+            {friend.name} owes you {Math.abs(friend.balance)}{" "}
+          </p>
+        )}
+
+        {friend.balance < 0 && (
+          <p className="red">
+            {" "}
+            You owe {friend.name} {Math.abs(friend.balance)}{" "}
+          </p>
+        )}
+
+        {friend.balance === 0 && <p> You and {friend.name} are even. </p>}
+      </div>
+
       <Button>Select</Button>
     </li>
-  )
+  );
 }
