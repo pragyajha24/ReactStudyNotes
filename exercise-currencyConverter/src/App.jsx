f2728fe5c14dfabba82f71632d5cdbd1bd46287e
Pragya Jha <pragyajha50@gmail.com>
2026-06-17 22:37:30 +0530

    building currency converter

--- exercise-currencyConverter/src/App.jsx
+++ exercise-currencyConverter/src/App.jsx
@@ -1,4 +1,59 @@
+import { useEffect, useState } from "react";
+
 // Currency Converter Exercise - Lecture -158
 export default function App() {
-  return <></>;
+  const [amount, setAmount] = useState(1);
+
+  const [fromCurrency, setFromCurrency] = useState("INR");
+  const [toCurrency, setToCurrency] = useState("USD");
+
+  const [isLoading, setisLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function convert() {
+        setisLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.dev/v1/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`,
+        );
+
+        const data = await res.json();
+        console.log(data);
+        setisLoading(false);
+      }
+
+      convert();
+    },
+    [amount, fromCurrency, toCurrency],
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="INR">INR</option>
+        <option value="CAD">CAD</option>
+      </select>
+
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="INR">INR</option>
+        <option value="CAD">CAD</option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  );
 }
