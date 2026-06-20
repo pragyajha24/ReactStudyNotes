d7526eba88736551696f7411207ef796c9a3780d
Pragya Jha <pragyajha50@gmail.com>
2026-06-20 19:26:25 +0530

    created a currency converter

--- exercise-currencyConverter/src/App.jsx
+++ exercise-currencyConverter/src/App.jsx
@@ -2,13 +2,22 @@ import { useEffect, useState } from "react";
 
 // Currency Converter Exercise - Lecture -158
 export default function App() {
+  //state for input field
   const [amount, setAmount] = useState(1);
 
-  const [fromCurrency, setFromCurrency] = useState("INR");
-  const [toCurrency, setToCurrency] = useState("USD");
+  //state for select - from currency
+  const [fromCurrency, setFromCurrency] = useState("USD");
 
+  //state for select -to currency
+  const [toCurrency, setToCurrency] = useState("INR");
+
+  //state to show the output on UI
+  const [converted, setConverted] = useState("");
+
+  //state for loading
   const [isLoading, setisLoading] = useState(false);
 
+  //use effect state for fetching data from api
   useEffect(
     function () {
       async function convert() {
@@ -18,10 +27,17 @@ export default function App() {
         );
 
         const data = await res.json();
-        console.log(data);
+        // console.log(data);
+        console.log(data.rates[toCurrency]);
+
+        setConverted(data.rates[toCurrency]);
+
         setisLoading(false);
       }
 
+      /* both currencies can't be same ,so if user has selected both currencies same
+      then putting a condition which will prevent convert() from running */
+      if (fromCurrency == toCurrency) return setConverted(amount);
       convert();
     },
     [amount, fromCurrency, toCurrency],
@@ -37,6 +53,7 @@ export default function App() {
       <select
         value={fromCurrency}
         onChange={(e) => setFromCurrency(e.target.value)}
+        disabled={isLoading}
       >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
@@ -47,13 +64,22 @@ export default function App() {
       <select
         value={toCurrency}
         onChange={(e) => setToCurrency(e.target.value)}
+        disabled={isLoading}
       >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="INR">INR</option>
         <option value="CAD">CAD</option>
       </select>
-      <p>OUTPUT</p>
+      <p>
+        {converted} {toCurrency}
+      </p>
     </div>
   );
 }
+
+/* 
+isLoading state is used in disabled prop of select elements
+, when fetching begins it will disable those and we can not change its value.
+
+*/
