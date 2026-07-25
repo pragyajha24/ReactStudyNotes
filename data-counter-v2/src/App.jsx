cf9679bfe85409dd40f42d72407c71265dc80514
Pragya Jha <pragyajha50@gmail.com>
2026-07-25 18:02:49 +0530

    version 2 of date counter

--- data-counter-v2/src/App.jsx
+++ data-counter-v2/src/App.jsx
@@ -1,5 +1,76 @@
 import { useState } from "react";
 
+//Version 2
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [range, setRange] = useState(1);
+
+  function countMinus() {
+    setCount(function (c) {
+      return c - range;
+    });
+  }
+
+  function countPlus() {
+    setCount(function (c) {
+      return c + range;
+    });
+  }
+
+  function handleReset() {
+    setCount(0);
+    setRange(1);
+  }
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div className="dataCounter">
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={range}
+          onChange={(e) => setRange(Number(e.target.value))}
+        />
+
+        <span>Step : {range}</span>
+      </div>
+
+      <div className="counts">
+        <button onClick={countMinus}>-</button>
+        {/* <p>Count : {count}</p> */}
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+
+        <button onClick={countPlus}>+</button>
+      </div>
+
+      <p>
+        {count === 0
+          ? `Today is `
+          : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        <span>{date.toDateString()}</span>
+      </p>
+
+      {count !== 0 || range !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+// Version 1
+/*
 export default function App() {
   return <Counter />;
 }
@@ -72,3 +143,4 @@ function Counter() {
     </div>
   );
 }
+ */
