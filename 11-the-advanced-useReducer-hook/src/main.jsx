aed8f5e5c86688d36470df76a602561c29491b14
Pragya Jha <pragyajha50@gmail.com>
2026-08-06 19:29:06 +0530

    setting react quiz application

--- 11-the-advanced-useReducer-hook/src/main.jsx
+++ 11-the-advanced-useReducer-hook/src/main.jsx
@@ -1,11 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+export default function Main({ children }) {
+  return <main className="main">{children}</main>;
+}
