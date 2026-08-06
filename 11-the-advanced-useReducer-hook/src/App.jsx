e6794798c79fdbf2692c5fd5e72cc1a87aa52347
Pragya Jha <pragyajha50@gmail.com>
2026-08-07 02:44:45 +0530

    created reducer function and added dispatch after getting data

--- 11-the-advanced-useReducer-hook/src/App.jsx
+++ 11-the-advanced-useReducer-hook/src/App.jsx
@@ -2,6 +2,30 @@ import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  //'loading', 'error','ready','active','finished';
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        question: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
@@ -10,9 +34,11 @@ export default function App() {
       try {
         const res = await fetch(`http://localhost:8000/questions`);
         const data = await res.json();
-        console.log(data);
+        // console.log(data);
+        dispatch({ type: "dataReceived", payload: data });
       } catch (err) {
         console.error("Error");
+        dispatch({ type: "dataFailed" });
       }
     }
     fetchQuestions();
