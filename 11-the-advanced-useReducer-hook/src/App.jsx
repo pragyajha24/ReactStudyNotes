ffddbe5a9cbcbe93b660176db9be2d7a0edf067c
Pragya Jha <pragyajha50@gmail.com>
2026-08-07 01:33:51 +0530

    added json file , installed json-server , and fetched questions data

--- 11-the-advanced-useReducer-hook/src/App.jsx
+++ 11-the-advanced-useReducer-hook/src/App.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch(`http://localhost:8000/questions`);
+        const data = await res.json();
+        console.log(data);
+      } catch (err) {
+        console.error("Error");
+      }
+    }
+    fetchQuestions();
+  }, []);
+
   return (
     <div className="app">
       <Header />
