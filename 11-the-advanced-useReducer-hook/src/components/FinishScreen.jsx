a1509497a041e55a66ac41695df4aa66fe0e513f
Pragya Jha <pragyajha50@gmail.com>
2026-08-24 03:00:00 +0530

    when we are at last question displaying finish button and showing finishScreen component

---
+++ 11-the-advanced-useReducer-hook/src/components/FinishScreen.jsx
@@ -0,0 +1,17 @@
+export default function FinishScreen({ points, maxPossiblePoints }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "🙃";
+  if (percentage >= 0 && percentage < 50) emoji = "😕";
+  if (percentage === 0) emoji = "🤦‍♀️";
+
+  return (
+    <p className="result">
+      <span>{emoji}</span> You scored <strong> {points} </strong> out of{" "}
+      {maxPossiblePoints} ({Math.ceil(percentage)}%)
+    </p>
+  );
+}
