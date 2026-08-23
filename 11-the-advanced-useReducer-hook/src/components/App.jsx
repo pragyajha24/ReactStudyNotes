a5a7a189ced2aa07fe4690b3bbb1f0b8936afa2f
Pragya Jha <pragyajha50@gmail.com>
2026-08-24 04:46:26 +0530

    setting a timerfeature  using useEffect

--- 11-the-advanced-useReducer-hook/src/components/App.jsx
+++ 11-the-advanced-useReducer-hook/src/components/App.jsx
@@ -11,6 +11,8 @@ import FinishScreen from "./FinishScreen";
 import Footer from "./Footer";
 import Timer from "./Timer";
 
+const SECS_PER_QUESTION = 30;
+
 const initialState = {
   questions: [],
   //'loading', 'error','ready','active','finished';
@@ -21,6 +23,7 @@ const initialState = {
   //points state
   points: 0,
   highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -42,6 +45,7 @@ function reducer(state, action) {
       return {
         ...state,
         status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
       };
 
     case "newAnswer":
@@ -76,6 +80,12 @@ function reducer(state, action) {
         highscore: state.highscore,
         status: "ready",
       };
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
     default:
       throw new Error("Action unknown");
   }
@@ -83,8 +93,10 @@ function reducer(state, action) {
 
 export default function App() {
   //destructing state object
-  const [{ questions, status, index, answer, points, highscore }, dispatch] =
-    useReducer(reducer, initialState);
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce(function (prev, cur) {
@@ -132,7 +144,7 @@ export default function App() {
               answer={answer}
             />
             <Footer>
-              <Timer />
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
 
               <NextButton
                 dispatch={dispatch}
--- 11-the-advanced-useReducer-hook/src/components/Timer.jsx
+++ 11-the-advanced-useReducer-hook/src/components/Timer.jsx
@@ -1,3 +1,25 @@
-export default function Timer() {
-  return <div className="timer">07:00</div>;
+import { useEffect } from "react";
+
+export default function Timer({ dispatch, secondsRemaining }) {
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        dispatch({ type: "tick" });
+      }, 1000);
+      return () => clearInterval(id);
+    },
+    [dispatch],
+  );
+
+  return (
+    <div className="timer">
+      {" "}
+      {mins < 10 && "0"}
+      {mins} : {seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
 }
