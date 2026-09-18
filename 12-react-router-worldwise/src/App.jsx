b62624f31ec32677c6fd03ca3c948ed710e5c831
Pragya Jha <pragyajha50@gmail.com>
2026-09-19 04:44:01 +0530

    learning global css into css module

--- 12-react-router-worldwise/src/App.jsx
+++ 12-react-router-worldwise/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
+import AppLayout from "./pages/AppLayout";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="/app" element={<AppLayout />} />
       </Routes>
     </BrowserRouter>
   );
--- 12-react-router-worldwise/src/pages/Homepage.jsx
+++ 12-react-router-worldwise/src/pages/Homepage.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 export default function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>Homepage - World Wise </h1>
+      <Link to="/app">Go to the App</Link>
     </div>
   );
 }
