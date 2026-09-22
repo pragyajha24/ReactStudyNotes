15e5a1de6fdfffbad80950f05ddb4cfae1d4ce43
Pragya Jha <pragyajha50@gmail.com>
2026-09-23 02:11:30 +0530

    nested routes using outlet element in sidebar layout

--- 12-react-router-worldwise/src/App.jsx
+++ 12-react-router-worldwise/src/App.jsx
@@ -5,6 +5,7 @@ import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
+import Form from "./components/Form";
 
 export default function App() {
   return (
@@ -14,7 +15,12 @@ export default function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
