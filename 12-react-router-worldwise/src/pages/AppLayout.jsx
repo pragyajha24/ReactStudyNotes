528eae8d0ef21b498aa48e9715393cc1738ce23c
Pragya Jha <pragyajha50@gmail.com>
2026-09-23 00:04:46 +0530

    creating app layout - sidebar, footer

--- 12-react-router-worldwise/src/pages/AppLayout.jsx
+++ 12-react-router-worldwise/src/pages/AppLayout.jsx
@@ -1,10 +1,10 @@
 import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
 
 export default function AppLayout() {
   return (
     <div>
-      <AppNav />
-      <p>app </p>
+      <Sidebar />
     </div>
   );
 }
