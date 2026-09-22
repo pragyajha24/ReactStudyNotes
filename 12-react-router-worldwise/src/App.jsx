2723b5094fb099ef84d3e1466a1fa4972d7f92c3
Pragya Jha <pragyajha50@gmail.com>
2026-09-22 21:04:28 +0530

    building the pages

--- 12-react-router-worldwise/src/App.jsx
+++ 12-react-router-worldwise/src/App.jsx
@@ -3,6 +3,8 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
@@ -11,7 +13,9 @@ export default function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="/app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
